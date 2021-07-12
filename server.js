919901d91520347625f6e3326c73f73331cd45c3
IgorChaikin <ighor.chaikin@gmail.com>
2021-07-12 12:14:46 +0300

    sepated requests for get categories list, items of each category and orders list. add request for post order

--- server.js
+++ server.js
@@ -16,106 +16,134 @@ app.use(express.static(path.join(__dirname, 'static/svg')));
 
 //test
 app.get('/ping', function (req, res) {
-    return res.send('pong');
+  return res.send('pong');
 });
 
 //html
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const data = {categories: [
-    {
-        id: '0',
-        title: 'Pizza',
-        items: [
-            {
-                id: '0-0',
-                imgPath: '/ham_and_cheese.png',
-                price: 26.75,
-                title: 'Ham and cheese',
-                description: 'Ham and cheese ham and cheese ham and cheese ham and cheese ham and cheese',
-                tags: []
-            },
-            {
-                id: '0-1',
-                imgPath: '/margarita.png',
-                price: 20.50,
-                title: 'Margarita',
-                description: 'Margarita margarita margarita margarita margarita margarita margarita',
-                tags: []
-            },
-            {
-                id: '0-2',
-                imgPath: '/pepperoni.png',
-                price: 20.50,
-                title: 'Pepperoni',
-                description: 'Pepperoni pepperoni pepperoni pepperoni pepperoni',
-                tags: []
-            },
-            {
-                id: '0-3',
-                imgPath: '/vegetable.png',
-                price: 17.30,
-                title: 'Vegetable',
-                description: 'Vegetable vegetable vegetable vegetable vegetable',
-                tags: ['0', '1']
-            }
-        ]
-    },
+  {
+    id: '0',
+    title: 'Pizza',
+    items: [
+      {
+        id: '0-0',
+        imgPath: '/ham_and_cheese.png',
+        price: 26.75,
+        title: 'Ham and cheese',
+        description: 'Ham and cheese ham and cheese ham and cheese ham and cheese ham and cheese',
+        tags: []
+      },
+      {
+        id: '0-1',
+        imgPath: '/margarita.png',
+        price: 20.50,
+        title: 'Margarita',
+        description: 'Margarita margarita margarita margarita margarita margarita margarita',
+        tags: []
+      },
+      {
+        id: '0-2',
+        imgPath: '/pepperoni.png',
+        price: 20.50,
+        title: 'Pepperoni',
+        description: 'Pepperoni pepperoni pepperoni pepperoni pepperoni',
+        tags: []
+      },
+      {
+        id: '0-3',
+        imgPath: '/vegetable.png',
+        price: 17.30,
+        title: 'Vegetable',
+        description: 'Vegetable vegetable vegetable vegetable vegetable',
+        tags: ['0', '1']
+      }
+    ]
+  },
 
-    {
-        id: '1',
-        title: 'Pasta',
-        items: []
-    },
+  {
+    id: '1',
+    title: 'Pasta',
+    items: []
+  },
 
-    {
-        id: '2',
-        title: 'Sandwiches',
-        items: []
-    },
+  {
+    id: '2',
+    title: 'Sandwiches',
+    items: []
+  },
 
-    {
-        id: '3',
-        title: 'Soup',
-        items: []
-    },
+  {
+    id: '3',
+    title: 'Soup',
+    items: []
+  },
 
-    {
-        id: '4',
-        title: 'Salads',
-        items: []
-    },
+  {
+    id: '4',
+    title: 'Salads',
+    items: []
+  },
 
-    {
-        id: '5',
-        title: 'Sides',
-        items: []
-    },
+  {
+    id: '5',
+    title: 'Sides',
+    items: []
+  },
 
-    {
-        id: '6',
-        title: 'Deserts',
-        items: []
-    },
+  {
+    id: '6',
+    title: 'Deserts',
+    items: []
+  },
 
-    {
-        id: '7',
-        title: 'Drinks',
-        items: []
-    },
+  {
+    id: '7',
+    title: 'Drinks',
+    items: []
+  },
 ],
 orders: {
-    ordered:[],
-    baking:[],
-    finishing:[],
-    served:[]
+  ordered:[],
+  baking:[],
+  finishing:[],
+  served:[]
 }};
 
-app.get('/data', function (request, response) {
-    //if(!request.body) return response.sendStatus(400);
-    response.json(data);
-})
+app.get('/categories', function (request, response) {
+  //if(!request.body) return response.sendStatus(400);
+  const categories = data.categories.map((elem) => {
+    return {
+      id: elem.id,
+      title: elem.title
+    };
+  })
+  response.json(categories);
+});
+
+app.get('/items', function (request, response) {
+  //if(!request.body) return response.sendStatus(400);
+  const id = request.query.id;
+  const selectedCategory = data.categories.find((elem)=>elem.id === id);
+  const items = {
+  title: selectedCategory.title,
+  items: selectedCategory.items
+  }
+  response.json(items);
+});
+
+app.get('/orders', function (request, response) {
+  //if(!request.body) return response.sendStatus(400);
+  response.json(data.orders);
+});
+
+app.post("/orders", function (request, response) {
+  if(!request.body) return response.sendStatus(400);
+  this.orders.ordered.push(request.body.item);
+  response.send(data.orders);
+});
 
 app.listen(port);
